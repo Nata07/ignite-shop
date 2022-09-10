0bf0b9912eff7bdf38c21aa44534a3f4461f1b1d
Natanael <nata.mw@hotmail.com>
2022-09-10 02:47:07 -0300

    added slider in list product

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,15 @@ import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 import { Container, Header } from "../styles/pages/app"
 import Image from 'next/future/image'
+import logo from '../assets/logo.svg'
 
 globalStyles()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src="" alt="Logo" />
+        <Image src={logo} alt="Logo" />
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,46 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
+import Image from "next/future/image"
+import {useKeenSlider} from 'keen-slider/react'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-})
+import camisa1 from '../assets/01.png'
+import camisa2 from '../assets/02.png'
+import camisa3 from '../assets/03.png'
+
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    }
+  })
   return (
-    <Button>Hello World</Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camisa1} width={480} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 01</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} width={480} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 01</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={480} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 01</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
